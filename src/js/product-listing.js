3cc3deb6ff1780cfd7f4bb8ea213f62584aca151
Johnathan Babb <johnathanwbabb@gmail.com>
2025-05-26 13:54:59 -0600

    Week 3 individual jwb

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,11 +1,12 @@
+import { loadHeaderFooter, getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
-import { loadHeaderFooter } from "./utils.mjs";
-
-const dataSource = new ProductData("tents");
-const element = document.querySelector(".product-list");
-
-const productList = new ProductList("Tents", dataSource, element);
-productList.init();
 
 loadHeaderFooter();
+
+const category = getParam("category");
+const dataSource = new ProductData();
+const element = document.querySelector(".product-list");
+const listing = new ProductList(category, dataSource, element);
+
+listing.init();
