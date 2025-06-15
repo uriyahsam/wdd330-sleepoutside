06e6b7ea665422b85f2fa264bfeb5f8b7bfea563
Caroline Pereira da Silva <carolinepedasil@gmail.com>
2025-06-15 17:53:47 -0300

    W05 Final Project: Report on Tasks

--- src/js/product.js
+++ src/js/product.js
@@ -1,5 +1,4 @@
 import { getParam } from './utils.mjs';
-import { setLocalStorage } from './utils.mjs';
 import ProductData from './ProductData.mjs';
 import ProductDetails from './ProductDetails.mjs';
 import { applyDiscountBadges } from './utils/price.js';
@@ -8,29 +7,7 @@ const productId = getParam('product');
 const dataSource = new ProductData('tents');
 const product = new ProductDetails(productId, dataSource);
 
-function getLocalStorage(key) {
-  const data = localStorage.getItem(key);
-  return data ? JSON.parse(data) : [];
-}
-
-function addProductToCart(productObj) {
-  const cart = getLocalStorage('so-cart');
-  cart.push(productObj);
-  setLocalStorage('so-cart', cart);
-}
-
-async function addToCartHandler(e) {
-  const id = e.target.dataset.id;
-  if (!id) return;
-  const productObj = await dataSource.findProductById(id);
-  addProductToCart(productObj);
-}
-
-document.addEventListener('DOMContentLoaded', async () => {
-  await product.init();
-
-  const btn = document.getElementById('addToCart');
-  if (btn) btn.addEventListener('click', addToCartHandler);
-
+document.addEventListener('DOMContentLoaded', () => {
+  product.init();
   applyDiscountBadges();
 });
